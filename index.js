031d07cb43669bb6f66f4df2c477ad7af242b1f7
David Mendelovits <david.mendelovits@gmail.com>
2018-01-10 20:54:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function doToElementsInArray(array, callback) {
   array.forEach(callback);
 }
@@ -10,4 +9,3 @@ function changeCompletely(element, index, array) {
 var animals = ["dog", "cat", "squirrel"];
 
 doToElementsInArray(animals, changeCompletely);
-
